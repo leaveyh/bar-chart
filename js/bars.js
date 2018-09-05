4215f0997b74632b1d7463d58d7038c7c44e44fa
HLL <leaveyh@gmail.com>
2018-09-05 17:44:26 -0400

    Adjustments

--- js/bars.js
+++ js/bars.js
@@ -36,7 +36,7 @@ var togNum1 = 1;
                         
         
                             var wChart = new dimple.chart(svg3, gradData1);
-                            wChart.setBounds("11%", "5%", "85%", "60%")
+                            wChart.setBounds("11%", "5%", "85%", "55%")
                             var edX = wChart.addCategoryAxis("x", ["College", "Program"]);
                             var eduY = wChart.addMeasureAxis("y", "Totals");
                            var edSeries = wChart.addSeries("Program", dimple.plot.bar);
@@ -45,7 +45,7 @@ var togNum1 = 1;
         
                             edX.addOrderRule("Montgomery College", "University of Baltimore", "Baltimore Community College");
         
-                            var qLegend = wChart.addLegend("15%", "80%", "40%", "95%");
+                            var qLegend = wChart.addLegend("15%", "80%", "40%", "85%");
                             
         
                        //     edX.addOrderRule(["Less than high school graduate", "High school graduate (includes equivalency)", "Some college or associate's degree", "Bachelor's degree or higher"]);
